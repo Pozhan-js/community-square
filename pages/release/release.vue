22255820e97343fccd0bc4976cb30b938c411cc3
Why so serious my dear <854059946@qq.com>
2023-07-18 17:24:46 +0800

    完成pages页面

--- pages/release/release.vue
+++ pages/release/release.vue
@@ -2,21 +2,91 @@
  * @Author: Why so serious my dear 854059946@qq.com
  * @Date: 2023-05-29 16:20:48
  * @LastEditors: Why so serious my dear 854059946@qq.com
- * @LastEditTime: 2023-07-18 10:33:12
+ * @LastEditTime: 2023-07-18 16:53:00
  * @FilePath: /community-square/pages/release/release.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-  <view> 发布 </view>
+  <view class="release padding-20">
+    <view
+      class="release-item flex-a-center-j-space-between"
+      v-for="(data, _) in releaseList"
+      :key="_"
+    >
+      <view class="release-item-left flex-a-center padding-20">
+        <image
+          style="width: 76rpx; height: 76rpx; margin-right: 20rpx"
+          :src="data.image"
+          mode=""
+        />
+        <view class="name">{{ data.name }}</view>
+      </view>
+      <view class="release-item-right">
+        <u-icon name="arrow-right" @click="toPath(data.path)"></u-icon>
+      </view>
+    </view>
+  </view>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      releaseList: [
+        {
+          image:
+            "https://kindoucloud.com:8077/api/mongoFile/Image/systemicon/SmartPark/20230718_debcdf99e98c4d54979dca29e4120263.png",
+          name: "房屋",
+          path: "./",
+        },
+        {
+          image:
+            "https://kindoucloud.com:8077/api/mongoFile/Image/systemicon/SmartPark/20230718_e5f75de8ced74c4080dbb6620b1dbedc.png",
+          name: "车务",
+          path: "./",
+        },
+        {
+          image:
+            "https://kindoucloud.com:8077/api/mongoFile/Image/systemicon/SmartPark/20230718_df3b4735ee3747059a8eda473d48362a.png",
+          name: "宠物",
+          path: "./",
+        },
+        {
+          image:
+            "https://kindoucloud.com:8077/api/mongoFile/Image/systemicon/SmartPark/20230718_d66525e920494d3988cffcf17db7209e.png",
+          name: "其他",
+          path: "./",
+        },
+      ],
+    };
+  },
+  methods: {
+    toPath(url) {
+      uni.navigateTo({
+        url,
+      });
+    },
   },
 };
 </script>
 
 <style lang="scss">
+.release {
+  width: 100vw;
+
+  &-item {
+    width: 100%;
+    // height: 100px;
+    // background-color: #fff;
+    // border-radius: 10px;
+    border-bottom: 1px solid #ccc;
+
+    .name {
+      height: 45rpx;
+      font-size: 32rpx;
+      color: #1f2329;
+      line-height: 45rpx;
+    }
+  }
+}
 </style>
--- pages/square/square.vue
+++ pages/square/square.vue
@@ -1,14 +1,112 @@
 <template>
-  <view> 广场</view>
+  <view class="square">
+    <view class="square-container padding-20">
+      <view
+        class="square-container-item"
+        v-for="(item, _) in squareList"
+        :key="_"
+      >
+        <image
+          style="width: 100%; height: 246rpx"
+          :src="item.imageUrl"
+          mode=""
+        />
+        <view class="title">{{ item.title }}</view>
+        <view class="type">{{ item.type }}</view>
+        <view class="price">{{ item.price }}</view>
+      </view>
+      <view class="square-container-item"></view>
+    </view>
+  </view>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      squareList: [
+        {
+          imageUrl:
+            "https://kindoucloud.com:8077/api/mongoFile/Image/systemicon/SmartPark/20230718_01f10eb6a5864f94ab7e232f699e51ae.png",
+          title: "猫狗寄养",
+          type: "寄养·洗澡",
+          price: "200元/天",
+        },
+        {
+          imageUrl:
+            "https://kindoucloud.com:8077/api/mongoFile/Image/systemicon/SmartPark/20230718_09bc78eeff804811b4d2cbfc0d17e1f4.png",
+          title: "宠物出售",
+          type: "幼犬柯基",
+          price: "2000元",
+        },
+        {
+          imageUrl:
+            "https://kindoucloud.com:8077/api/mongoFile/Image/systemicon/SmartPark/20230718_6d0a7cd929024d588f9fbb348f58c22c.png",
+          title: "整租 | 精装修押一付一",
+          type: "3室·120㎡·长江友邻·一单元",
+          price: "4000元/月",
+        },
+        {
+          imageUrl:
+            "https://kindoucloud.com:8077/api/mongoFile/Image/systemicon/SmartPark/20230718_6c1eaf2e73d946e6a1db48c0a7c6bcc2.png",
+          title: "出售 | 三室一厅精装房",
+          type: "3室·120㎡·长江友邻·一单元",
+          price: "150万",
+        },
+        {
+          imageUrl:
+            "https://kindoucloud.com:8077/api/mongoFile/Image/systemicon/SmartPark/20230718_6c922c4dc8124c32910fb00cedff85f1.png",
+          title: "私家车位 | 亏本出售",
+          type: "A区6栋A155号",
+          price: "8万",
+        },
+        {
+          imageUrl:
+            "https://kindoucloud.com:8077/api/mongoFile/Image/systemicon/SmartPark/20230718_9a630e0859854889bc2dab34567d02e1.png",
+          title: "minicooper   2018款",
+          type: "米色 自动挡",
+          price: "19.98万",
+        },
+      ],
+    };
   },
 };
 </script>
 
 <style lang="scss">
+.square {
+  width: 100vw;
+
+  &-container {
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 20px;
+
+    &-item {
+      border-radius: 20rpx 20rpx 0 0;
+
+      .title {
+        margin: 10rpx 0;
+        font-size: 32rpx;
+        font-weight: bold;
+        color: #333333;
+      }
+
+      .type {
+        margin: 10rpx 0;
+        font-size: 24rpx;
+        color: #333333;
+      }
+
+      .price {
+        height: 40rpx;
+        font-size: 28rpx;
+        font-weight: bold;
+        color: #ff0000;
+        line-height: 40rpx;
+      }
+    }
+  }
+}
 </style>
