01c79a554eed3b9c0123c5358ed1bc640f023a33
Why so serious my dear <854059946@qq.com>
2023-07-20 16:53:11 +0800

    完成地址页面

--- subPages/square/edit-address.vue
+++ subPages/square/edit-address.vue
@@ -1,110 +1,235 @@
-<!--
- * @Author: Why so serious my dear 854059946@qq.com
- * @Date: 2023-07-20 15:41:32
- * @LastEditors: Why so serious my dear 854059946@qq.com
- * @LastEditTime: 2023-07-20 16:18:34
- * @FilePath: /community-square/subPages/square/edit-address.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
-
 <template>
-  <view class="edit-address">
-    <!-- 注意，如果需要兼容微信小程序，最好通过setRules方法设置rules规则 -->
-    <u--form labelPosition="left" :model="model1" :rules="rules" ref="uForm">
-      <u-form-item label="姓名" prop="userInfo.name" borderBottom ref="item1">
-        <u--input v-model="model1.userInfo.name" border="none"></u--input>
-      </u-form-item>
-      <u-form-item
-        label="性别"
-        prop="userInfo.sex"
-        borderBottom
-        @click="
-          showSex = true;
-          hideKeyboard();
-        "
-        ref="item1"
-      >
-        <u--input
-          v-model="model1.userInfo.sex"
-          disabled
-          disabledColor="#ffffff"
-          placeholder="请选择性别"
-          border="none"
-        ></u--input>
-        <u-icon slot="right" name="arrow-right"></u-icon>
-      </u-form-item>
-    </u--form>
-    <u-action-sheet
-      :show="showSex"
-      :actions="actions"
-      title="请选择性别"
-      description="如果选择保密会报错"
-      @close="showSex = false"
-      @select="sexSelect"
-    >
-    </u-action-sheet>
+  <view>
+    <view class="editaddress">
+      <view class="content">
+        <view class="row">
+          <view class="nominal">收货人</view>
+          <view class="input"
+            ><input
+              placeholder="请输入收货人姓名"
+              v-model="addressData.name"
+              type="text"
+            />
+          </view>
+        </view>
+        <view class="row">
+          <view class="nominal">电话号码</view>
+          <view class="input">
+            <input
+              placeholder="请输入收货人电话号码"
+              v-model="addressData.phone"
+              type="number"
+              maxlength="11"
+            />
+          </view>
+        </view>
+        <view class="row">
+          <view class="nominal">所在地区</view>
+          <view class="input selectcity" @tap="openPicker">
+            <input
+              placeholder="请选择省市区"
+              disabled
+              type="text"
+              v-model="addressData.address"
+            />
+            <image src="/static/images/home/right.png" class="rights"></image>
+          </view>
+        </view>
+        <view class="row">
+          <view class="nominal">详细地址</view>
+          <view class="input">
+            <textarea
+              style="font-size: 28upx"
+              v-model="addressData.moreAddres"
+              auto-height="true"
+              placeholder="输入详细地址"
+              v-if="show == false"
+            ></textarea>
+          </view>
+        </view>
+        <view class="row">
+          <view class="nominal" style="color: #999; margin-top: 10upx"
+            >设为默认地址</view
+          >
+          <view class="input switch">
+            <switch
+              :color="colors"
+              style="transform: scale(0.8)"
+              @change="switchChange"
+              :checked="addressData.isdefult == 1"
+            ></switch>
+          </view>
+        </view>
+      </view>
+      <view class="save">
+        <view class="btn" :style="'background:' + colors">保存地址</view>
+      </view>
+
+      <!-- 省市区选择 show:是否显示  @sureSelectArea：确认事件 @hideShow：隐藏事件-->
+      <cc-selectDity
+        :show="show"
+        @sureSelectArea="onsetCity"
+        @hideShow="onhideShow"
+      ></cc-selectDity>
+    </view>
   </view>
-</template
-  ></view>
 </template>
 
 <script>
+var app = getApp();
+
 export default {
   data() {
     return {
-      showSex: false,
-      model1: {
-        userInfo: {
-          name: "",
-          sex: "",
-        },
+      colors: "",
+      show: false,
+      addressData: {
+        name: "",
+        phone: "",
+        address: "",
+        moreAddres: "",
+        isdefult: 0,
       },
-      actions: [
-        {
-          name: "男",
-        },
-        {
-          name: "女",
-        },
-        {
-          name: "保密",
-        },
-      ],
-      rules: {
-        "userInfo.name": {
-          type: "string",
-          required: true,
-          message: "请填写姓名",
-          trigger: ["blur", "change"],
-        },
-        "userInfo.sex": {
-          type: "string",
-          max: 1,
-          required: true,
-          message: "请选择男或女",
-          trigger: ["blur", "change"],
-        },
-      },
-      radio: "",
-      switchVal: false,
+      isShow: true,
     };
   },
   methods: {
-    sexSelect(e) {
-      this.model1.userInfo.sex = e.name;
-      this.$refs.uForm.validateField("userInfo.sex");
+    openPicker() {
+      console.log("执行打开地址选择器");
+      this.show = true;
     },
-  },
-  onReady() {
-    //如果需要兼容微信小程序，并且校验规则中含有方法等，只能通过setRules方法设置规则。
-    this.$refs.uForm.setRules(this.rules);
+
+    onhideShow() {
+      this.show = false;
+      console.log("执行了关闭地址选择器");
+    },
+    //选中省市区
+    onsetCity(e) {
+      let data = e.detail.target.dataset;
+      let address = data.province + data.city + data.area;
+      this.show = false;
+      this.addressData.address = address;
+    },
+    switchChange(e) {
+      //   this.setData({
+      //     checked: e.detail.value,
+      //   });
+    },
   },
 };
 </script>
-
-
 <style lang="scss" scoped>
-.edit-address {
-  width: 100vw;
+.save {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 120upx;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.save view {
+  display: flex;
+}
+
+.save .btn {
+  box-shadow: 0upx 5upx 10upx rgba(0, 0, 0, 0.4);
+  width: 70%;
+  height: 80upx;
+  border-radius: 80upx;
+  background-color: #5991fa;
+  color: #fff;
+  justify-content: center;
+  align-items: center;
+  font-size: 30upx;
+  margin-bottom: 60px;
+}
+
+.save .btn .icon {
+  height: 80upx;
+  color: #fff;
+  font-size: 30upx;
+  justify-content: center;
+  align-items: center;
+}
+
+.content {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 40upx;
+}
+
+.content view {
+  display: flex;
+}
+
+.content .row {
+  width: 92%;
+  margin: 0 4%;
+  border-bottom: solid 1upx #eee;
+}
+
+.content .row .nominal {
+  width: 30%;
+  height: 80upx;
+  font-size: 28upx;
+  font-family: Droid Sans Fallback;
+  font-weight: 400;
+  color: rgba(51, 51, 51, 1);
+  align-items: center;
+}
+
+.content .row .input {
+  width: 70%;
+  padding: 20upx 0;
+  align-items: center;
+  font-size: 28upx;
+}
+
+.content .row .input input {
+  font-size: 28upx;
+  color: #333333;
+}
+
+.content .row .switch {
+  justify-content: flex-end;
+}
+
+.content .row .input textarea {
+  min-height: 40upx;
+  line-height: 40upx;
+}
+
+.content .del_box {
+  width: 100%;
+  display: block;
+  overflow: hidden;
+  margin-right: 30upx;
+}
+
+.content .del {
+  width: 240upx;
+  height: 80upx;
+  float: right;
+  justify-content: center;
+  align-items: center;
+  font-size: 28upx;
+  color: #f23a3a;
+  margin: 0 auto;
+  margin-top: 50upx;
+  border-radius: 38upx;
+  background-color: rgba(255, 0, 0, 0.05);
+  border-bottom: solid 1upx #eee;
+}
+
+.selectcity input {
+  width: 90%;
+}
+
+.selectcity image {
+  width: 40upx;
+  height: 40upx;
+  float: right;
 }
 </style>
