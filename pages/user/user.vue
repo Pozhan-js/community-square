2a6e98f9765094d5aeb8c99071e8219f58f9bd7a
Why so serious my dear <854059946@qq.com>
2023-07-24 09:34:17 +0800

    完成登录功能:zap:

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -1,40 +1,88 @@
 <template>
   <view class="user-pages container">
-    <navbar pages />
+    <!-- <navbar pages title="我的" bgColor="#5991fa" :custom="true" /> -->
     <view
       class="user-pages-header relative padding-y-24 flex"
-      :class="{ 'items-center': !isLogin }">
+      :class="{ 'items-center': !isLogin }"
+    >
+      <!-- 头像 -->
       <u-avatar size="140rpx" :src="avatar" />
+      <!-- 登录后展示 -->
       <view v-if="isLogin" class="user-info flex-col flex-grow justify-evenly">
+        <!-- 电话展示 -->
         <text class="user-name text_ellipsis">{{ realName }}</text>
         <text class="study-days">学习{{ studyDays }}天</text>
       </view>
+      <!-- 未登录展示 -->
       <view
         v-else
         class="login-btn color-white bg-theme radius-25"
-        @click="handleLogin">
+        @click="handleLogin"
+      >
         点击登录
       </view>
       <image
         class="head-bg absolute top-0"
-        :src="
-          $helper.getImage('', '20220829_9950d6ac472c4ad7bfe80a4d5511c3bf')
-        "></image>
+        :src="$helper.getImage('', '20220829_9950d6ac472c4ad7bfe80a4d5511c3bf')"
+      ></image>
     </view>
+
+    <!-- 我的页面功能 -->
+    <view class="user-types flex-a-center-j-space-around">
+      <view class="user-types-item">
+        <view class="num">2</view>
+        <view>我发布的</view>
+      </view>
+      <view class="user-types-item">
+        <view class="num">2</view>
+        <view>我发布的</view>
+      </view>
+      <view class="user-types-item">
+        <view class="num">2</view>
+        <view>我发布的</view>
+      </view>
+    </view>
+
+    <!-- 卡片 -->
     <view
-      class="user-pages-body bg-white radius-10 overflow-hidden padding-y-24 margin-t-24">
-      <u-cell-group :border="false">
-        <u-cell
-          v-for="(item, index) in cellList"
+      class="user-pages-body bg-white radius-10 overflow-hidden padding-24 margin-t-24"
+    >
+      <view
+        class="user-pages-body-top flex-a-center-j-space-between margin-b-24"
+      >
+        <view>我的订单</view>
+        <view class="flex-a-center">
+          <view>查看</view>
+          <u-icon name="arrow-right"></u-icon>
+        </view>
+      </view>
+
+      <view class="user-pages-body-content flex-a-center-j-space-between">
+        <view
+          class="content-item"
+          v-for="(item, index) in contentList"
+          @click="handleBodyItemClick(item.path)"
           :key="index"
-          isLink
-          center
-          size="large"
-          :icon="item.icon"
-          :title="item.title"
-          @click="handleCellClick(item)" />
-      </u-cell-group>
+        >
+          <image style="width: 44rpx; height: 56rpx" :src="item.icon" mode="" />
+          <view>{{ item.title }}</view>
+        </view>
+      </view>
     </view>
+
+    <!-- 功能列表页 -->
+    <u-cell-group :border="false">
+      <u-cell
+        v-for="(item, index) in cellList"
+        :key="index"
+        isLink
+        center
+        size="large"
+        :icon="item.icon"
+        :title="item.title"
+        @click="handleCellClick(item)"
+      />
+    </u-cell-group>
   </view>
 </template>
 
@@ -43,12 +91,41 @@ import infoMixin from "@/common/mixins/info";
 export default {
   mixins: [infoMixin],
   data() {
-    const getIcon = icon => `/static/image/icon/user/${icon}.png`;
-    const getPath = path => `/subPages/user/${path}/${path}`;
+    const getIcon = (icon) => `/static/image/icon/user/${icon}.png`;
+    const getPath = (path) => `/subPages/user/${path}/${path}`;
 
     return {
       cellList: [
         { title: "设置", path: getPath("settings"), icon: getIcon("settings") },
+        {
+          title: "地址管理",
+          path: getPath("address"),
+          icon: getIcon("address"),
+        },
+        { title: "消息", path: getPath("message"), icon: getIcon("message") },
+      ],
+
+      contentList: [
+        {
+          title: "全部",
+          path: getPath("all-order"),
+          icon: getIcon("all-order"),
+        },
+        {
+          title: "待支付",
+          path: getPath("payment-order"),
+          icon: getIcon("payment-order"),
+        },
+        {
+          title: "已完成",
+          path: getPath("success-order"),
+          icon: getIcon("success-order"),
+        },
+        {
+          title: "已取消",
+          path: getPath("cancel-order"),
+          icon: getIcon("cancel-order"),
+        },
       ],
     };
   },
@@ -61,19 +138,28 @@ export default {
     /**
      * @description: 跳转登录页
      * @return {*}
-     */    
+     */
     handleLogin() {
       this.$jump("/subPages/user/login/login");
     },
 
     /**
-     * @description: 
+     * @description:
      * @param {*} item
      * @return {*}
-     */    
+     */
     handleCellClick(item) {
       if (!item.path) this.$message("功能开发中...", "none");
       else this.$jump(item.path);
+    },
+
+    /**
+     * @description:
+     * @param {*} path
+     * @return {*}
+     */
+    handleBodyItemClick(path) {
+      this.$jump(path);
     },
   },
 };
@@ -81,8 +167,27 @@ export default {
 <style lang="scss" scoped>
 .user-pages {
   padding: 0 5%;
+  height: 100vh;
+  position: relative;
+  > view {
+    position: relative;
+    z-index: 1;
+  }
+
+  &::after {
+    content: "";
+    // display: block;
+    height: 544rpx;
+    background-color: #5991fa;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 0;
+  }
+
   &-header {
-    margin-top: 16rpx;
+    margin-top: 200rpx;
     ::v-deep > .u-avatar {
       margin-right: 36rpx;
     }
@@ -109,6 +214,33 @@ export default {
       height: 100%;
     }
   }
+
+  .user-types {
+    font-size: 24rpx;
+    color: #ffffff;
+
+    &-item {
+      text-align: center;
+
+      .num {
+        height: 70rpx;
+        font-size: 50rpx;
+        font-weight: 800;
+        color: #ffffff;
+        line-height: 70rpx;
+      }
+    }
+  }
+
+  &-body {
+    font-size: 26rpx;
+    color: #333333;
+
+    .content-item {
+      text-align: center;
+    }
+  }
+
   ::v-deep .u-cell:last-child > .u-cell__body + view {
     // 隐藏最后一个下划线
     display: none;
